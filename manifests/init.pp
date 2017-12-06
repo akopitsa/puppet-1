9dbc7754314d23ac484fe98867d8d84abc046c0e
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-06 15:24:06 +0200

    change to function

--- manifests/init.pp
+++ manifests/init.pp
@@ -60,7 +60,7 @@ class ssh(
 String $package_name,
 String $service_name,
 String $ensure,
-Boolean $service_enable,
+String $service_enable,
 String $service_ensure,
 Boolean $permit_root_login = false,
 Integer $port              = 22,
