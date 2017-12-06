74d6dbacbcc17d8382c896a1089c693c40cac157
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-06 15:50:02 +0200

    Add

--- manifests/init.pp
+++ manifests/init.pp
@@ -60,7 +60,7 @@ class ssh(
 String $package_name,
 String $service_name,
 String $ensure,
-String $service_enable,
+Boolean $service_enable,
 String $service_ensure,
 Boolean $permit_root_login = false,
 Integer $port              = 22,
