9a8337e0506906d1f88a81ac566ebb507d9eabf5
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-06 17:17:29 +0200

    no message

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,10 +1,15 @@
+# Class: ssh:service
+#
 class ssh::service(
-  String $service_name = $::ssh::params::service_name,
+# String $service_name   = $::ssh::params::service_name,  
+  String $service_name   = $::ssh::service_name,
+  String $service_ensure = $::ssh::service_ensure,
+  Boolean $service_enable = $::ssh::service_enable
 ) {
   service {'ssh-service':
-    ensure     => running,
+    ensure     => $service_ensure,
     name       => $service_name,
-    enable     => true,
+    enable     => $service_enable,
     hasstatus  => true,
     hasrestart => true,
   }
