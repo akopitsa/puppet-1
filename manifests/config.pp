0076e3238c0962df7228b0242e56908cdc455066
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-06 11:25:21 +0200

    Config

---
+++ manifests/config.pp
@@ -0,0 +1,12 @@
+# Class: nassh::config
+#
+class ssh::config {
+  # resources
+  file { '/etc/ssh/sshd_config':
+    ensure => 'file',
+    mode => '0600',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///modules/ssh/files/sshd_config'
+  }
+}
